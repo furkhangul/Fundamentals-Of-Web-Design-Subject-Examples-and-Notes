449ee3aa83d46e53117504c7fc30a6e90eb84e66
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-06-03 18:12:16 +0300

    1

---
+++ Final/NodeJS/0.1 NodeJS Nedir.js
@@ -0,0 +1,20 @@
+/*
+Node JS nedir ?
+
+->Node.js, JavaScript dilini tarayıcı dışında, özellikle sunucu tarafında çalıştırmak için kullanılan açık kaynaklı, platformlar arası bir çalışma ortamıdır.
+
+->Node.js bir compiler yani derleyicidir.
+
+->JavaScript kullanarak yalnızca front-end geliştirme yapabiliyorken Node.js ile artık back-end 
+     geliştirme yapmakta mümkün kılınmıştır. Çünkü Js anlayıp derleyebilen bir compiler artık tarayıcıda tüm senaryo 
+     dönmektedir.
+|--->Buna en iyi örnek Node ile server tarafında uygulama geliştirebilmekteyiz.
+
+->Temel programlama dillerinde, dil önce derlenir ardından çıktı verir veya hata varsa uyarı verir.
+Node.js diğer diller gibi çıktı ve hataların göz önüne almak için mutlaka bir derleyiciye ihtiyaç duyar.
+
+->Hızlıdır, çok sayıda kullanıcıya aynı anda cevap verir.
+
+->Tek dil ile tam bir web uygulaması yapılabilir.
+
+*/
