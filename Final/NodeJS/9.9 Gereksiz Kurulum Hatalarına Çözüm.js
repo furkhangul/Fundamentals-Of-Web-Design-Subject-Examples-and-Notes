9a69a25fd39e4d162676a1d4ee5a60246d14d26d
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-06-03 18:31:41 +0300

    99

---
+++ Final/NodeJS/9.9 Gereksiz Kurulum Hatalarına Çözüm.js
@@ -0,0 +1,37 @@
+/*
+Bu dosyayı normal çalıştırırsak js kodu olarak çalışır fakat biz node üzerinden çalıştığımız için terminal üzerinden açacağız.
+Bunun için node'un kendi web sitesinden indirip cmd açıp node -v kodunu yazıp varlığını kontrol ediyoruz.
+Ardından VS Code kullanıyorum ben bunun için de Code'da sol alttaki terminal bölümüne gidip terminal türü olarak 
+GİT BASH seçiyoruz.
+
+Bash Terminali yoksa:
+https://git-scm.com/downloads/win
+indir ve Powershell gibi listelerin olduğu yerden bash seçeneğine tıkla.
+ */
+
+
+/*
+hâlâ olmadıysa ve hata alıyorsak son olarak:
+
+Ortam Değişkenine (PATH) Ekle:
+
+ Adımlar:
+
+Windows arama çubuğuna “Ortam değişkenlerini düzenle” yaz ve aç.
+
+Açılan pencerede:
+
+Altta: Ortam Değişkenleri (Environment Variables) butonuna tıkla.
+
+Üstte Kullanıcı Değişkenleri bölümünde Path seç → Düzenle (Edit) → Yeni (New) de
+
+Şunu ekle:
+
+makefile
+Kopyala
+Düzenle
+C:\Program Files\nodejs\
+Eğer farklı bir klasöre kuruluysa, oraya git → adres çubuğundan kopyala → yukarıdaki gibi ekle.
+
+*/
+console.log("Merhaba Dünya")
